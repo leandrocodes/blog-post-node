5a7efd6dc477c54652bbcc893a878a3066c441d4
leandrocodes <leaxviana140@gmail.com>
2019-11-14 22:16:47 -0400

    post route

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('we are on home')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('we are on posts')
 })
 
 //ROUTES HERE
