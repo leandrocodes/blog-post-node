5c446e909f3632b3e68d71d8714d9e01d78970e9
leandrocodes <leaxviana140@gmail.com>
2019-11-16 02:16:49 -0400

    better file structure

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
 const express = require('express')
+const functions = require('firebase-functions')
 const bodyParser = require('body-parser')
 
-admin.initializeApp(functions.config().firebase)
-const db = admin.firestore()
+const firebase = require('./firebase')
+const routes = require('./routes')
 
 const app = express()
 const main = express()
+
+app.use(routes)
 
 app.use((req, res, next) => {
   const apiKey = req.get('API-Key')
@@ -20,122 +21,7 @@ app.use((req, res, next) => {
 
 main.use('/api/v1', app)
 main.use(bodyParser.json())
-
-exports.dev = functions.https.onRequest(main)
-
-app.get('/warmup', (req, res) => {
-  res.send('Warming up friend.')
-})
-
-app.post('/products', async (req, res) => {
-  try {
-
-    const { name, brand, price, quantity } = req.body
-
-    const data = {
-      name,
-      brand,
-      price,
-      quantity
-    }
-
-    const productsRef = await db.collection('products').add(data)
-    const product = await productsRef.get()
-
-    res.json({
-      id: productsRef.id,
-      data: product.data()
-    })
-
-  } catch (err) {
-    res.status(500).send(err)
-  }
-})
-
-app.get('/products', async (req, res) => {
-  try {
-
-    const productRef = await db.collection('products').get()
-    const products = []
-
-    productRef.forEach(doc => {
-      products.push({
-        id: doc.id,
-        data: doc.data()
-      })
-    })
-
-    res.json(products)
-
-  } catch (err) {
-    res.status(500).send(err)
-  }
-})
-
-app.get('/products/:id', async (req, res) => {
-  try {
-    const productId = req.params.id
-
-    if (!productId) throw new Error('ID is required')
-
-    const product = await db.collection('products').doc(productId).get()
-
-    if (!product.exists) {
-      throw new Error(`Product doesn't exist.`)
-    } else {
-      res.json({
-        id: product.id,
-        data: product.data()
-      })
-    }
-  } catch (err) {
-    res.status(500).send(err)
-  }
-})
-
-app.patch('/products/:id', async (req, res) => {
-  try {
-
-    const productId = req.params.id
-    if (!productId) throw new Error('ID is required')
-
-    const data = req.body
-    if (!data) throw new Error('Data is needed')
-
-    const product = await db.collection('products').doc(productId).get()
-    if (!product.exists) {
-      throw new Error('Product not exists')
-    } else {
-      db.collection('products').doc(productId).update(data)
-
-      res.json({
-        id: productId,
-        data: product.data()
-      })
-    }
-  } catch (err) {
-    res.sendStatus(500).send(err)
-  }
-})
+/* app.use(firebase)  */
 
 
-app.delete('/products/:id', async (req, res) => {
-  try {
-
-    const productId = req.params.id
-    if (!productId) throw new Error('ID is required')
-
-    const product = await db.collection('products').doc(productId).get()
-    if (!product.exists) {
-      throw new Error('Product not exists')
-    } else {
-      db.collection('products').doc(productId).delete()
-
-      res.json({
-        id: productId
-      })
-    }
-  } catch (err) {
-    res.sendStatus(500).send(err)
-  }
-})
+exports.tests = functions.https.onRequest(main)
---
+++ functions/routes.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('./firebase')
+
+router.get('/warmup', (req, res) => {
+  res.send('Warming up friend.')
+})
+
+router.post('/products', async (req, res) => {
+  try {
+
+    const { name, brand, price, quantity } = req.body
+
+    const data = {
+      name,
+      brand,
+      price,
+      quantity
+    }
+
+    const productsRef = await db.collection('products').add(data)
+    const product = await productsRef.get()
+
+    res.json({
+      id: productsRef.id,
+      data: product.data()
+    })
+
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.get('/products', async (req, res) => {
+  try {
+
+    const productRef = await db.collection('products').get()
+    const products = []
+
+    productRef.forEach(doc => {
+      products.push({
+        id: doc.id,
+        data: doc.data()
+      })
+    })
+
+    res.json(products)
+
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.get('/products/:id', async (req, res) => {
+  try {
+    const productId = req.params.id
+
+    if (!productId) throw new Error('ID is required')
+
+    const product = await db.collection('products').doc(productId).get()
+
+    if (!product.exists) {
+      throw new Error(`Product doesn't exist.`)
+    } else {
+      res.json({
+        id: product.id,
+        data: product.data()
+      })
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.patch('/products/:id', async (req, res) => {
+  try {
+
+    const productId = req.params.id
+    if (!productId) throw new Error('ID is required')
+
+    const data = req.body
+    if (!data) throw new Error('Data is needed')
+
+    const product = await db.collection('products').doc(productId).get()
+    if (!product.exists) {
+      throw new Error('Product not exists')
+    } else {
+      db.collection('products').doc(productId).update(data)
+
+      res.json({
+        id: productId,
+        data: product.data()
+      })
+    }
+  } catch (err) {
+    res.sendStatus(500).send(err)
+  }
+})
+
+
+router.delete('/products/:id', async (req, res) => {
+  try {
+
+    const productId = req.params.id
+    if (!productId) throw new Error('ID is required')
+
+    const product = await db.collection('products').doc(productId).get()
+    if (!product.exists) {
+      throw new Error('Product not exists')
+    } else {
+      db.collection('products').doc(productId).delete()
+
+      res.json({
+        id: productId
+      })
+    }
+  } catch (err) {
+    res.sendStatus(500).send(err)
+  }
+})
+
+
+module.exports = router
