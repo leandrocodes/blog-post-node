8eeda79c1bab0a3b9e38cbd244e5946e3bcbb3f3
leandrocodes <leaxviana140@gmail.com>
2019-12-15 12:44:13 -0400

    hello route

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 let router = express.Router()
 
 router.use('/products', require('./products'))
+router.use('/hello', require('./hello'))
 
 module.exports = router
